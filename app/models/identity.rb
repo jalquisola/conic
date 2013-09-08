cd9cd65c551e251232d075aebc218b57d85cb74a
joseph <jralquisola@gmail.com>
2013-09-08 16:52:29 +0800

    fix regexp

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -2,5 +2,5 @@ class Identity < ActiveRecord::Base
   #attr_accessible :email, :name, :password_digest
   validates_presence_of :name
   validates_uniqueness_of :email
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\Z/i
 end
