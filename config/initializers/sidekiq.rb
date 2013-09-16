c2704ebb025094c866fe14e5e1737ba3d2ef690c
joseph <jralquisola@gmail.com>
2013-09-16 21:03:43 +0800

    update config examples

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,14 @@
 Sidekiq.configure_server do |config|
   config.redis = {
     :url => "redis://#{REDIS_CONFIG[:host]}:#{REDIS_CONFIG[:port]}",
-    :namespace => SIDEKIQ_CONFIG[:namespace] || "sidekiq"
+    :namespace => "sidekiq"
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
     :url => "redis://#{REDIS_CONFIG[:host]}:#{REDIS_CONFIG[:port]}",
-    :namespace => SIDEKIQ_CONFIG[:namespace] || "sidekiq"
+    :namespace => "sidekiq"
   }
 end
 
-# Reconfigure sidekiq options for action mailer extension
-#class Sidekiq::Extensions::DelayedMailer
-#  sidekiq_options :retry => 3
-#end
