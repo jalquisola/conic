6251c54d066eb3b5b48b2124e41890c8a2a79b66
joseph <jralquisola@gmail.com>
2013-09-16 22:18:00 +0800

    disable social links

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,22 +1,12 @@
 module UsersHelper
   def create_social_links(enabled_links)
-    socials = ["dailymile", "foursquare", "path", "facebook", "twitter"]
+    #socials = ["dailymile", "foursquare", "path", "facebook", "twitter"]
+    socials = ["dailymile"]
     partial_html = []
-    #partial_html << "<ul class='nav nav-pills nav-stacked'>"
     socials.each do |social| 
       link = "/auth/#{social}/callback"
       link = "https://api.dailymile.com/oauth/authorize?response_type=code&client_id=#{DAILYMILE_CONFIG['CLIENT_ID']}&redirect_uri=http://conic.dev/auth/dailymile/callback" if social == "dailymile"
 
-      #partial_html << <<-HTML
-      #  <li class=''>
-      #    <a href='#{link}'>
-      #      <span class='badge pull-right'>
-      #        #{enabled_links.include?(social) ? 'ON' : 'OFF'}
-      #      </span>
-      #      #{social.capitalize}
-      #    </a>
-      #  </li>
-      #HTML
       partial_html << <<-HTML
         <div class="panel panel-info">
           <div class="panel-heading">
